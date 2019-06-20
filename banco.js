f01856636cf20d046fdc065bc0c3780350686a98
vitorhugoinf <40009988+vitorhugoinf@users.noreply.github.com>
2019-06-20 18:18:58 -0300

    Add files via upload

--- banco.js
+++ banco.js
@@ -1,17 +1,18 @@
 let busca=document.querySelector('.busca');
 let it=document.querySelector('.item');
 let resu=document.querySelector('.resultado');
+let descri=document.querySelector('.descri');
 let linha= document.createElement('br')
- let objeto={
+ let dados={
        feijão:{
-        descricao:'O feijão contem em uma porção 5,4mg '+document.write('<br>')+' de sodio,37mg de calcio ,3 mg de ferro',
+        descricao:'O feijão contem em uma porção:'+'</br>' +' 5.4 mg de sodio'+'</br>'+ ' 37mg calcio ' +'</br>'+'3 mg de ferro',
         },
         carne:{
-            descricao:'Ferro - 4,3mg/100g',
-            descricao:'Fósforo - 276mg/100g,Potássio - 334mg/100g,Sódio - 69mg/100g,Magnésio - 24,5mg/100g,Cálcio - 5,4mg/100g,Contém também cloro, enxofre e silício',
+            descricao:'Fósforo - 276mg'+'</br>'+'Potássio - 334mg'+'</br>'+'Sódio - 69mg,Magnésio'+'</br>'+ ' 24,5mg,Cálcio - 5,4mg'+'</br>'+'Contém também cloro, enxofre e silício',
         },
-        wilson:{
-            descricao:'ok',
+        strogonoff:{
+            descricao:'frango ou carne',
+            composicao:'peito de frango(ou carne)'+'</br>'+'alho'+'</br>'+'maionese'+'</br>'+'maionese'+'</br>'+'manteiga'+'</br>'+'ketchup'+'</br>'+'mostarda'+'</br>'+'creme de leite',
         },
         vitor:{
             descricao:'falso',
@@ -20,11 +21,9 @@ let linha= document.createElement('br')
 
 
 busca.addEventListener('click',function(){
-    console.log(objeto[it.value].descricao);
-    if(objeto[it.value]==' '){
-        resu.innerHTML='Não temos não';
-    }
-    else{
-    resu.innerHTML=objeto[it.value].descricao;
-    }
+    /*so para mostra no console pra ver o q tem*/
+    console.log(dados[it.value].descricao);
+    /*aqui adiciona o objeto pesquisado*/
+    resu.innerHTML=dados[it.value].descricao;
+    descri.innerHTML=dados[it.value].composicao;
 });
