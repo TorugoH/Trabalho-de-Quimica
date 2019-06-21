73622801b918bd7b0c0fa214e3da2ef1ebb8b6b2
vitorhugoinf <40009988+vitorhugoinf@users.noreply.github.com>
2019-06-20 22:14:49 -0300

    Add files via upload

--- banco.js
+++ banco.js
@@ -4,24 +4,27 @@ let resu=document.querySelector('.resultado');
 let descri=document.querySelector('.descri');
 let nome=document.querySelector('.nome');
  let dados={
-       feijão:{
-        descricao:'O feijão contem em uma porção:'+'</br>' +' 5.4 mg de sodio'+'</br>'+ ' 37mg calcio ' +'</br>'+'3 mg de ferro',
-        },
-        carne:{
-            descricao:'Fósforo'+'</br>'+'Potássio'+'</br>'+'Sódio,Magnésio'+'</br>'+ 'Cálcio'+'</br>'+'Contém também cloro, enxofre e silício',
-        },
         strogonoff:{
             name:'frango ou carne',
             composicao:'peito de frango(ou carne)'+'</br>'+
             'alho'+'</br>'+'maionese'+'</br>'+'maionese'+
             '</br>'+'manteiga'+'</br>'+'ketchup'+'</br>'+'mostarda'+'</br>'+'creme de leite',
-            descricao:'Na carne contem: Fósforo,Potássio,Sódio,Magnésio,Cálcio.Contém também cloro, enxofre e silício'+
-            '</br>'+'Para a composição da mionese podemos ver em '+
-            '<a href="https://pt.wikipedia.org/wiki/Maionese">maionese</a>'+'</br>',
+            descricao:'Quando você ingere 100g de strogonofe de carne voçê esta consumondo:'+'</br>'
+            +'28g de Calcio'+'</br>'
+            +'22g de Magnesio'+'</br>'
+            +'186mg de Fosforo'+'</br>'
+            +'2.7g de Ferro'+'</br>'
+            +'2g de Zinco'+'</br>',
         },
-        v:{
-            descricao:'',
+        bife_de_panela:{
+            name:'bife de panela',
+            composicao:'',
+            descricao:'Quando você ingere 100g de strogonofe de carne voçê esta consumondo:'+'</br>'
+            +'Sódio 255mg'+'</br>'
+            +'Potássio 319mg'+'</br>',
         },
+        
+        
     };
 
 
